7615717c3757abd6d688fe6f2c400181b653c023
3ll34ndr0 <3ll34ndr0@github.com>
2017-06-13 16:48:30 -0300

    lista

---
+++ apse/tp/2/lista.c
@@ -0,0 +1,20 @@
+/*
+Una lista es una estructura de datos lineal en donde las in-
+serciones y supresiones se pueden realizar en cualquier posición de la
+lista. Teniendo en cuenta la definición antes mencionada implemente
+:w
+una lista de enteros con las siguientes operaciones:
+• inicializar(l): inicializa la lista. El programador determina los
+valores de inicialización.
+• insertar(l,e,p): inserta en la lista l el elemento e en la posición p.
+• suprimir(l,p): elimina de la lista l el elemento que se encuentra
+en la posición p. Suprimir devuelve como resultado el elemento
+eliminado.
+• vacío(l): esta funcion devuelve verdadero si la lista l está vacía.
+Falso en otro caso.
+• lleno(l): esta función devuelve verdadero si la lista está llena.
+Falso en otro caso.
+• imprimir(l): imprime la lista.
+• recuperarElemento(l,p): retorna como resultado el elemento de l
+que se encuentra en la posición p.
+*/
