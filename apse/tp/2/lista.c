ac2c98e1a2a3583558f18a238aeaaf866df59ca5
3ll34ndr0 <3ll34ndr0@github.com>
2017-06-22 13:30:40 -0300

    temporal

--- apse/tp/2/lista.c
+++ apse/tp/2/lista.c
@@ -1,3 +1,6 @@
+#include "lista.h"
+#include <stdlib.h>
+
 /*
 Una lista es una estructura de datos lineal en donde las in-
 serciones y supresiones se pueden realizar en cualquier posición de la
@@ -18,3 +21,40 @@ Falso en otro caso.
 • recuperarElemento(l,p): retorna como resultado el elemento de l
 que se encuentra en la posición p.
 */
+
+
+Lista * inicializar(){
+   Lista * lista            = (Lista*)malloc(sizeof(Lista));
+   lista->cabeza            = (Nodo*)malloc(sizeof(Nodo));
+   lista->cabeza->siguiente = 0; //Null pointer
+   setDatoNodo((lista->cabeza), 0);//valor de prueba
+   lista->cola              = lista->cabeza; //Cola apunta a cabeza
+   return lista;
+}
+
+/* Para acceder a los miembros de la lista */
+int sizeOfLista(Lista * l){
+//	if(!vacio(l));
+	int i=1;
+	while(lista-cabeza->siguiente!=0){i++;}
+	return i;
+}
+int getHeadOfLista(Lista * l){
+	return getDatoNodo(l->cabeza);
+}
+int getColaOfLista(Lista * l){
+	return getDatoNodo(l->cola);
+}
+bool lleno(Lista * l){
+	if(l->cabeza->siguiente==0);
+
+
+
+/* Get and Set para el tipo de dato Nodo */
+int getDatoNodo(Nodo * n){
+	return n->dato;
+}
+void setDatoNodo(Nodo * n, int dato){
+	n->dato = dato;
+}
+
