cb8e64a99eb157fde810d6ed3143a8fcd91015a7
3ll34ndr0 <3ll34ndr0@github.com>
2018-04-17 12:48:30 -0300

    Agrego ejercicios 8 y 9

---
+++ pase/tp/tp2/src/ejercicio8.sh
@@ -0,0 +1,14 @@
+# Ejercicio 8: Escriba un script que defina la función apliqueATodos. Esta función recibe
+# como parámetros un comando simple (comando con un parámetro y sin opciones) y una lista
+# de archivos. La función aplica el comando a cada uno de los archivos recibidos como parámetros.
+
+apliqueATodos () {
+    comando=$1
+    #echo "Antes digo $algo"
+    shift
+    while [ -n "$1" ]
+    do
+        $comando $1
+        shift
+    done
+}
---
+++ pase/tp/tp2/src/ejercicio9.sh
@@ -0,0 +1,43 @@
+DIRECTORIO=$1
+ARCHIVO_BUS=$2
+shift 2
+PALABRAS=$*
+echo "Cantidad de parámetros: $#"
+ if [ $# -ge 3 ]
+      then
+         ls $DIRECTORIO | grep $ARCHIVO_BUS | while read ARCHIVO
+                                                 do
+                                                    grep "$PALABRAS" ${DIRECTRIO}/${ARCHIVO}
+                                                 done
+      else
+         echo "Número de argumentos insuficientes"
+         echo "Use: $0 <directorio> <archivo_a_buscar> <palabras>"
+ fi
+
+ # Este script recibe 3 parámetros: Un directorio, un archivo y una o más palabras. Supuestamente
+ # busca esas palabra en el archivo de ese directorio, si la encuentra nos dice en que líneas aparece.
+ #
+ # El problema es que nunca hace eso, ya que la condición de test nunca se cumple, debido
+ # al shift 2. Por lo tanto para que funcione el test sería:
+ # [ $# -ge 1 ]
+ # Además hay un error de tipeo cuando usamos la variable DIRECTORIO en la línea 10. Corrijo los dos bugs
+ # y el script queda así:
+
+ejercicio9Corregido () {
+DIRECTORIO=$1
+ARCHIVO_BUS=$2
+shift 2
+PALABRAS=$* # Agrupa todos los parámetros restantes en una sola variable.
+if [ $# -ge 1 ] # Luego del shift 2 deberían quedar al menos un parámetro (la/s palabra/s a buscar)
+      then
+         ls $DIRECTORIO | grep $ARCHIVO_BUS | while read ARCHIVO
+                                                 do
+                                                    echo "grep \"$PALABRAS\" ${DIRECTRIO}/${ARCHIVO}"
+                                                    grep "$PALABRAS" ${DIRECTORIO}/${ARCHIVO}
+                                                 done
+      else
+         echo "Número de argumentos insuficientes"
+         echo "Use: $0 <directorio> <archivo_a_buscar> <palabras>"
+ fi
+
+}
